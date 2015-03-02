df9d08b4bb588f4f378a3b97cabe8fdcefead698
Oliver Hoffman <oliverhoffmanx0@yahoo.com>
2015-03-02 18:35:52 -0500

    Successfull declaration of create work function

--- lab2/test.c
+++ lab2/test.c
@@ -9,20 +9,22 @@ struct one_work {
 
 one_work_t **make_work(int num_works) {
     one_work_t **work_array;
-    *work_array = (one_work_t *)malloc(num_works * sizeof(one_work_t));
-    // int i;
-    // for (i = 0; i < num_works; i++) {
-    //     one_work_t *w;
-    //     w->sz = 3;
-    //     int *array = (int *)malloc(w->sz * sizeof(int));
-    //     array[0] = 1;
-    //     array[1] = 2;
-    //     array[2] = 3;
-    //     w->array = array;
-    //     *work_array[i] = *w;
-    // }
+    work_array = (one_work_t **)malloc(num_works * sizeof(one_work_t*));
+    int i;
+    for (i = 0; i < num_works; i++) {
+         one_work_t *w = (one_work_t*)malloc(sizeof(one_work_t));
+         w->sz = 3+i;
+         w->array = (int*)malloc((w->sz)*sizeof(int));
+         int j;
+         for(j = 0; j < w->sz; ++j)
+         {
+            w->array[j] = i;
+         }
+         work_array[i] = w;
+        //w->array = array;
+        work_array[i] = w;
+   }
     // printf("parameter received: %d\n", num_works);
-    printf("HH\n");
 
     return work_array;
 }
@@ -31,7 +33,20 @@ int main (int argc, char **argv) {
 
     printf("hello there buddy\n");
     mw.create_work_pool = make_work;
-    mw.create_work_pool(4);
+    one_work_t ** test = mw.create_work_pool(4);
+
+    int i = 0;
+    for(i = 0; i < 4; ++i)
+    {
+      //  printf("This is array: %d\n", test[i]->sz);
+        printf("This is array: %d\n", i);
+        int j = 0;
+        for(j = 0; j < test[i]->sz; ++j)
+        {
+            printf("Array elem %d: %d\n",j, test[i]->array[j]);
+        }
+    }
+
     //make_work(1);
 
     return 0;
