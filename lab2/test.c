212f9014b0bef6e20b0322b1a0eddadc5776dc82
Oliver Hoffman <oliverhoffmanx0@yahoo.com>
2015-03-02 17:49:16 -0500

    test.c updated

--- lab2/test.c
+++ lab2/test.c
@@ -8,19 +8,21 @@ struct one_work {
 } w;
 
 one_work_t **make_work(int num_works) {
-    one_work_t **work_array = (one_work_t *)malloc(num_works * sizeof(one_work_t));
-    int i;
-    for (i = 0; i < num_works; i++) {
-        one_work_t w;
-        w.sz = 3;
-        int *array = (int *)malloc(w.sz * sizeof(int));
-        array[0] = 1;
-        array[1] = 2;
-        array[2] = 3;
-        w.array = array;
-        work_array[i] = w;
-    }
-    printf("parameter received: %d\n", num_works);
+    one_work_t **work_array;
+    *work_array = (one_work_t *)malloc(num_works * sizeof(one_work_t));
+    // int i;
+    // for (i = 0; i < num_works; i++) {
+    //     one_work_t *w;
+    //     w->sz = 3;
+    //     int *array = (int *)malloc(w->sz * sizeof(int));
+    //     array[0] = 1;
+    //     array[1] = 2;
+    //     array[2] = 3;
+    //     w->array = array;
+    //     *work_array[i] = *w;
+    // }
+    // printf("parameter received: %d\n", num_works);
+    printf("HH\n");
 
     return work_array;
 }
@@ -29,8 +31,8 @@ int main (int argc, char **argv) {
 
     printf("hello there buddy\n");
     mw.create_work_pool = make_work;
-
-    make_work(1);
+    mw.create_work_pool(4);
+    //make_work(1);
 
     return 0;
 }
