bd311e024cdd515d4be596fb2a8370d7dd199bc8
Oliver Hoffman <oliverhoffmanx0@yahoo.com>
2015-03-10 16:46:53 -0400

    added report

--- lab2/run.sh
+++ lab2/run.sh
@@ -4,7 +4,7 @@ rm factor_run
 mpicc api.c factor.c -lgmp -o factor_run
 
 # Run the tests
-for i in `seq 2 3`;
+for i in `seq 2 16`;
     do
         echo "Running with $i processors on multiple: 2626088838234521" >> $1
         { time mpirun -np $i -hostfile hosts factor_run 2626088838234521 >> $1; } 2>> $1
