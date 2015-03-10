44d499199ce1980061ba7505799ae8771dfc0dd0
Oliver Hoffman <oliverhoffmanx0@yahoo.com>
2015-03-09 22:38:07 -0400

    Default comment

--- lab2/api.c
+++ lab2/api.c
@@ -163,154 +163,212 @@
 
 
 
-
-// Runs master worker, sending out all work round-robbin style
 void MW_Run (int argc, char **argv, struct mw_fxns *f){
-	
-    // Counts number of messages sent (for granularity)
-    unsigned long num_msgs = 0;
 
     // Initialization of parameters
     int sz, myid;
     MPI_Comm_size (MPI_COMM_WORLD, &sz);
-  	MPI_Comm_rank (MPI_COMM_WORLD, &myid); 
+    MPI_Comm_rank (MPI_COMM_WORLD, &myid); 
 
-  	// master process sets up calculation
-  	if(myid == MASTER)
+
+    if(myid == MASTER)
     {
-      // Create all the work to be done
-    	one_work_t **work_chunks = f->create_work_pool(argc, argv);
+        one_work_t **work_chunks = f->create_work_pool(argc, argv);
 
-      // Send out the work to workers round-robbin style
-      int i = 0;
-      int process_num = 1;
-      int work_chunk_count = 0;
-      while(work_chunks[i] != NULL) {
+        int i = 0;
+        int process_num = 1;
+        while (work_chunks[i] != NULL)
+        {
+            one_work_t work_chunk = work_chunks[i];
+            debug_print("MASTER: Sending chunk to process %d out of %d\n", process_num, sz);
+            MPI_Send(work_chunk, f->work_sz, MPI_CHAR, process_num, WORK_TAG, MPI_COMM_WORLD);
+            ++process_num;
 
-        work_chunk_count++;
-        i++;
-        // // get the next work chunk
-        // one_work_t *work_chunk = work_chunks[i++];
+            if(process_num < sz)
+            {
+                process_num = 1;
+            }
 
-        // // send the chunk to a processor, roundrobbin style
-        // debug_print("Process %d out of %d\n", process_num, sz);
-        // MPI_Send(work_chunk, f->work_sz, MPI_CHAR, process_num,
-        //   WORK_TAG, MPI_COMM_WORLD);
-        // num_msgs++;
+            ++i;
+        }
+        debug_print("MASTER: Finished sending chunks. Telling processes to stop running.\n");
+        //Tell workers to finish running
+        for (process_num = 1; j < sz; ++j)
+        {
+            one_work_t work_chunk = (one_work_t*)malloc(sizeof(one_work_t*));
+            MPI_Send(work_chunk, f->work_sz, MPI_CHAR, process_num, DONE_TAG, MPI_COMM_WORLD);
+        }
+    }
+    else
+    {
+        int j = 0;
+        while(j == 0)
+        {
+            one_work_t work_chunk = (one_work_t*)malloc(sizeof(one_work_t*));
+            MPI_Status status;
+            debug_print("PROCESS %d: Receiving message from master\n", myid);
+            MPI_Recv(work_chunk, f->work_sz, MPI_CHAR, MASTER, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-        // if (++process_num >= sz) { // make sure we're roundrobbining.
-        //   process_num = 1;
-        // }
-
-        
-      }
-
-      debug_print("Number of chunks %d", work_chunk_count);
-      // //This was allocated in the user's do_work function
-      // for(i = 0; i < work_chunk_count; ++i)
-      // {
-      //     free(work_chunks[i]);
-      // }
-      // free(work_chunks);
-
-      // //We receive results..
-      // one_result_t **result_array = (one_result_t**)malloc(sizeof(one_result_t*)*work_chunk_count);
-      // if (!result_array) {
-      //     printf("MW library failed to allocate result_array on the heap\n");
-      //     exit(1);
-      // }
-      // for(i = 0; i < work_chunk_count; ++i)
-      // {
-      //     one_result_t *result = (one_result_t*)malloc((f->result_sz));
-      //     if (!result) {
-      //         printf("MW Librar failed to allocate a result struct on the heap\n");
-      //         exit(1);
-      //     }
-      //     MPI_Status status;
-
-      //     MPI_Recv(result, f->result_sz, MPI_CHAR, MPI_ANY_SOURCE, RESULT_TAG, 
-      //       MPI_COMM_WORLD, &status);
-      //     num_msgs++;
-
-      //     result_array[i] = result;
-      // }
-
-      // //Tell workers to stop running
-      // debug_print("MASTER telling workers to stop running\n");
-      // for (i = 1; i < sz; ++i)
-      // {
-      //     int num = 1;
-      //     MPI_Send(&num, 1, MPI_INT, i, DONE_TAG, MPI_COMM_WORLD);
-      //     num_msgs++;
-      // }
-
-      // //Report result!
-      // f->report_results(work_chunk_count, result_array);
-      // printf("Reporting from master. Num Msgs sent by all processors: %lu\n", num_msgs);
-
-      // // Free memeory of results array
-      // for(i = 0; i < work_chunk_count; ++i)
-      // {
-      //     free(result_array[i]);
-      // }
-      // free(result_array);
+            if (status.MPI_TAG == WORK_TAG)
+            {
+                 debug_print("PROCESS %d: The message was a work chunk!\n", myid);
+            }
+            else
+            {
+                debug_print("PROCESS %d: The message was to end!\n", myid);
+                j = 1;
+            }
+        } 
     }
 
-    // workers do work
-    // else
-    // {
-    //   debug_print("Hola, desde processor %d\n", myid);
-    //   int i = 1;
-    //   int work_chunk_count = 0;
-
-    //   //We create array to store results
-    //   one_result_t **result_array = (one_result_t**)malloc(sizeof(one_result_t*));
-
-    //   while(i > 0) {
-    //       one_work_t *work_chunk = (one_work_t *)malloc(f->work_sz);
-    //       if (!work_chunk) {
-    //           printf("Failed to allocate space for a work chunk on a worker\n");
-    //           exit(1);
-    //       }
-
-    //       MPI_Status status;
-    //       MPI_Recv(work_chunk, f->work_sz, MPI_CHAR, MASTER, MPI_ANY_TAG, 
-    //         MPI_COMM_WORLD, &status);
-    //       ++work_chunk_count;
-
-    //       if (status.MPI_TAG == WORK_TAG) 
-    //       {
-    //           realloc(result_array, sizeof(one_result_t*)*work_chunk_count);
-    //           one_result_t *result = f->do_one_work(work_chunk);
-    //           result_array[work_chunk_count-1] = result;
-    //           free(result);
-    //       } 
-    //       else if(status.MPI_TAG == DONE_TAG)
-    //       {
-    //           i = -1;
-    //           debug_print("Worker %d says: He terminado!\n", myid);
-    //       }
-    //       else
-    //       {
-    //          printf("Unexpected Error... Process %d received message with unknown tag.\n", myid);
-    //       }
-    //       free(work_chunk);
-
-    //   }
-
-    //   debug_print("Sending results from worker %d\n", myid);
-    //   for(i = 0; i < work_chunk_count; ++i)
-    //   {
-    //       one_result_t *result = result_array[i];
-    //       MPI_Send(result, f->result_sz, MPI_CHAR, MASTER, 
-    //             RESULT_TAG, MPI_COMM_WORLD);
-    //   }
-
-
-    // }
-    // debug_print("Finished Running on processor %d\n", myid);
+    debug_print("PROCESS %d: Finished execution!\n", myid);
 
 }
+
+// // Runs master worker, sending out all work round-robbin style
+// void MW_Run (int argc, char **argv, struct mw_fxns *f){
+	
+//     // Counts number of messages sent (for granularity)
+//     unsigned long num_msgs = 0;
+
+//     // Initialization of parameters
+//     int sz, myid;
+//     MPI_Comm_size (MPI_COMM_WORLD, &sz);
+//   	MPI_Comm_rank (MPI_COMM_WORLD, &myid); 
+
+//   	// master process sets up calculation
+//   	if(myid == MASTER)
+//     {
+//       // Create all the work to be done
+//     	one_work_t **work_chunks = f->create_work_pool(argc, argv);
+
+//       // Send out the work to workers round-robbin style
+//       int i = 0;
+//       int process_num = 1;
+//       int work_chunk_count = 0;
+//       while(work_chunks[i] != NULL) {
+
+//         // get the next work chunk
+//         one_work_t *work_chunk = work_chunks[i];
+
+//         // send the chunk to a processor, roundrobbin style
+//         debug_print("Sending chunk to process %d out of %d\n", process_num, sz);
+//         MPI_Send(work_chunk, f->work_sz, MPI_CHAR, process_num, WORK_TAG, MPI_COMM_WORLD);
+//         num_msgs++;
+
+//         // if (++process_num >= sz) { // make sure we're roundrobbining.
+//         //   process_num = 1;
+//         // }
+
+        
+//       }
+
+//       debug_print("Number of chunks %d", work_chunk_count);
+//       // //This was allocated in the user's do_work function
+//       // for(i = 0; i < work_chunk_count; ++i)
+//       // {
+//       //     free(work_chunks[i]);
+//       // }
+//       // free(work_chunks);
+
+//       // //We receive results..
+//       // one_result_t **result_array = (one_result_t**)malloc(sizeof(one_result_t*)*work_chunk_count);
+//       // if (!result_array) {
+//       //     printf("MW library failed to allocate result_array on the heap\n");
+//       //     exit(1);
+//       // }
+//       // for(i = 0; i < work_chunk_count; ++i)
+//       // {
+//       //     one_result_t *result = (one_result_t*)malloc((f->result_sz));
+//       //     if (!result) {
+//       //         printf("MW Librar failed to allocate a result struct on the heap\n");
+//       //         exit(1);
+//       //     }
+//       //     MPI_Status status;
+
+//       //     MPI_Recv(result, f->result_sz, MPI_CHAR, MPI_ANY_SOURCE, RESULT_TAG, 
+//       //       MPI_COMM_WORLD, &status);
+//       //     num_msgs++;
+
+//       //     result_array[i] = result;
+//       // }
+
+//       // //Tell workers to stop running
+//       // debug_print("MASTER telling workers to stop running\n");
+//       // for (i = 1; i < sz; ++i)
+//       // {
+//       //     int num = 1;
+//       //     MPI_Send(&num, 1, MPI_INT, i, DONE_TAG, MPI_COMM_WORLD);
+//       //     num_msgs++;
+//       // }
+
+//       // //Report result!
+//       // f->report_results(work_chunk_count, result_array);
+//       // printf("Reporting from master. Num Msgs sent by all processors: %lu\n", num_msgs);
+
+//       // // Free memeory of results array
+//       // for(i = 0; i < work_chunk_count; ++i)
+//       // {
+//       //     free(result_array[i]);
+//       // }
+//       // free(result_array);
+//     }
+
+//     // workers do work
+//     // else
+//     // {
+//     //   debug_print("Hola, desde processor %d\n", myid);
+//     //   int i = 1;
+//     //   int work_chunk_count = 0;
+
+//     //   //We create array to store results
+//     //   one_result_t **result_array = (one_result_t**)malloc(sizeof(one_result_t*));
+
+//     //   while(i > 0) {
+//     //       one_work_t *work_chunk = (one_work_t *)malloc(f->work_sz);
+//     //       if (!work_chunk) {
+//     //           printf("Failed to allocate space for a work chunk on a worker\n");
+//     //           exit(1);
+//     //       }
+
+//     //       MPI_Status status;
+//     //       MPI_Recv(work_chunk, f->work_sz, MPI_CHAR, MASTER, MPI_ANY_TAG, 
+//     //         MPI_COMM_WORLD, &status);
+//     //       ++work_chunk_count;
+
+//     //       if (status.MPI_TAG == WORK_TAG) 
+//     //       {
+//     //           realloc(result_array, sizeof(one_result_t*)*work_chunk_count);
+//     //           one_result_t *result = f->do_one_work(work_chunk);
+//     //           result_array[work_chunk_count-1] = result;
+//     //           free(result);
+//     //       } 
+//     //       else if(status.MPI_TAG == DONE_TAG)
+//     //       {
+//     //           i = -1;
+//     //           debug_print("Worker %d says: He terminado!\n", myid);
+//     //       }
+//     //       else
+//     //       {
+//     //          printf("Unexpected Error... Process %d received message with unknown tag.\n", myid);
+//     //       }
+//     //       free(work_chunk);
+
+//     //   }
+
+//     //   debug_print("Sending results from worker %d\n", myid);
+//     //   for(i = 0; i < work_chunk_count; ++i)
+//     //   {
+//     //       one_result_t *result = result_array[i];
+//     //       MPI_Send(result, f->result_sz, MPI_CHAR, MASTER, 
+//     //             RESULT_TAG, MPI_COMM_WORLD);
+//     //   }
+
+
+//     // }
+//     // debug_print("Finished Running on processor %d\n", myid);
+
+// }
 
 
 
