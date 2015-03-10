ace9d2f025186ce84ad1a25224e65386bbf3ba57
Faiyam Rahman <frahman305@gmail.com>
2015-03-10 11:43:46 -0400

    still fiddling with run.sh

--- lab2/run.sh
+++ lab2/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-for i in `seq 2 16`;
+# Remove the old version and compile a new one
+rm factor_run
+mpicc api.c factor.c -lgmp -o factor_run
+
+# Run the tests
+for i in `seq 2 3`;
     do
-        echo "Running with $i processors on multiple: 2626088838234521"
-        time mpirun -np $i -hostfile hosts factor_run 2626088838234521
+        echo "Running with $i processors on multiple: 2626088838234521" >> $1
+        time mpirun -np $i -hostfile hosts factor_run 2626088838234521  >> $1 2>&1
     done
