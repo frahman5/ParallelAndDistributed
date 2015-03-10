7014df71c461109246eecff35a3be47fe413086e
Oliver Hoffman <oliverhoffmanx0@yahoo.com>
2015-03-10 01:53:25 -0400

    Make factor.c run MW_Run2

--- lab2/factor.c
+++ lab2/factor.c
@@ -198,7 +198,7 @@ int main (int argc, char **argv) {
     MPI_Init (&argc, &argv);
 
     // run the program
-    MW_Run (argc, argv, &mw);
+    MW_Run2 (argc, argv, &mw);
 
     MPI_Finalize();
     
